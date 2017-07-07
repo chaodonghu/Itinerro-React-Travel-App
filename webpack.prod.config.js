d52d36be053c84173610b78d53c95203a6410743
Dong Hu <cdhu@uwaterloo.ca>
2017-07-06 20:41:13 -0400

    production build and server

---
+++ webpack.prod.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    // 'webpack-hot-middleware/client?reload=true',
+    'react-hot-loader/patch',
+    './react_clientside/src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    // publicPath: 'http://localhost:5000/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: [['es2015', {'modules': false}], 'react', 'stage-0'],
+        plugins: [
+          'lodash']
+      }
+    },  {
+      test: /\.s?css$/,
+      use: [ 'style-loader',
+        {
+          loader:'css-loader',
+          options: {
+            sourceMap: 'true',
+            importLoaders: 1
+          }
+        },
+        {
+          loader: 'sass-loader' // compiles Sass to CSS
+        },
+      ]
+    }],
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+};
