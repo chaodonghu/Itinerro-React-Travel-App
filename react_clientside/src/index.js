fdb0af11f1247712737ef5ef88c08b9c5daf8c04
Dong Hu <cdhu@uwaterloo.ca>
2017-07-06 20:52:14 -0400

    attempt to fix build path

--- react_clientside/src/index.js
+++ react_clientside/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import './style/scaffolds.scss';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
